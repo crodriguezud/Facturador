847d02cd5e43394096d607828eca2e1a62d4d0d7
Cristian Rodriguez <crodriguezud@gmail.com>
2017-10-31 22:02:38 -0500

    Prueba Docker

--- facturador/run_web.sh
+++ facturador/run_web.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python manage.py migrate --settings=umusicfy.settings.prd
-python manage.py collectstatic --no-input --settings=umusicfy.settings.prd
-/usr/local/bin/gunicorn umusicfy.wsgi:application -w 2 -b :8000
+python manage.py migrate --settings=facturador.settings
+python manage.py collectstatic --no-input --settings=facturador.settings
+/usr/local/bin/gunicorn facturador.wsgi:application -w 2 -b :8000
